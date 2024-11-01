466cdebbd433be0cb0fa6c8df8698fe5f767ec04
Mani <maniramkr2006@gmail.com>
2024-11-01 14:13:44 +0530

    repeat playing conditions added RPS and continue or quit

--- project_python/Rock_paper_scissor_Game.py
+++ project_python/Rock_paper_scissor_Game.py
@@ -15,50 +15,61 @@ class RPS(Enum):
 # print(RPS.ROCK.value)
 # print(RPS["SCISSOR"])
 
+playagain = True # adding logics if the player repeat the game
+while playagain:
+       
+       # print(" ")
 
-print(" ")
-
-playerchoice = input("Enter...\n1 for Rock\n2 for Scissor\n3 for Paper:\n\n")
+       playerchoice = input("Enter...\n1 for Rock\n2 for Scissor\n3 for Paper:\n\n")
 
 # printing 1,2,3 and choices Rock,Scissor,paper                                                 
 # print(playerchoice)  # choice 1 printed
 # print(type(playerchoice)) # type is string, but we need integer
 
-player = int(playerchoice) # datatype converted into string to integer
+       player = int(playerchoice) # datatype converted into string to integer
 
-if player < 1 or player > 3: # if player choice lessthan 1 or more then 3 
-# the lofic error ask system to exit from the programme with error message
-   sys.exit("😨 you must enter 1, 2, or 3.")
-
-
-computerchoice =random.choice("123") 
-# computer choicing randomly 123 as string
-computer =int(computerchoice) #datatype converted into integer from string
-
-# Hold the below printing method for include RPS value alternate method
-# print("you chose" + " " + playerchoice + " .")
-# print("python chose"+ " "+ computerchoice + ".")
-
-# instead of value sending the variable as value passing in RPS
-# to convert the datatype from enum to string use constructor str
-# use method replace for remove printing RPS in result
-
-print("you chose" + " " + str(RPS(player)).replace("RPS.", " ")+ " .") 
-print("python chose"+ " "+ str(RPS(computer)).replace("RPS.", " ") + ".")
+       if player < 1 or player > 3:
+                             
+              # if player choice lessthan 1 or more then 3 
+       # the lofic error ask system to exit from the programme with error message
+              s.exit("😨 you must enter 1, 2, or 3.")
 
 
-if player==1 and computer ==3:
-       print("🎉 you win")
-elif player ==2 and computer == 1:
-       print("🎯 you win")
-elif player ==3 and computer == 2:
-       print("😎 you win")
-elif player == computer:
-       print("🦋 Tie Game")
-else:
-       print("🏆 python win...!!!")
+       computerchoice =random.choice("123") 
+       # computer choicing randomly 123 as string
+       computer =int(computerchoice) #datatype converted into integer from string
+
+       # Hold the below printing method for include RPS value alternate method
+       # print("you chose" + " " + playerchoice + " .")
+       # print("python chose"+ " "+ computerchoice + ".")
+
+       # instead of value sending the variable as value passing in RPS
+       # to convert the datatype from enum to string use constructor str
+       # use method replace for remove printing RPS in result
+
+       print("you chose" + " " + str(RPS(player)).replace("RPS.", " ")+ " .") 
+       print("python chose"+ " "+ str(RPS(computer)).replace("RPS.", " ") + ".")
 
 
+       if player==1 and computer ==3:
+              print("🎉 you win")
+       elif player ==2 and computer == 1:
+              print("🎯 you win")
+       elif player ==3 and computer == 2:
+              print("😎 you win")
+       elif player == computer:
+              print("🦋 Tie Game")
+       else:
+              print("🏆 python win...!!!")
+# playagain variable used for the next playing options to player
+       playagain = input("\nplayagain ...?\ny for yes or \nQ for quit\n\n")
+       if playagain.lower() == "y":
+              continue
+       else:
+              print("\n 🌹🌹🌹🌹")
+              print("Thank you for playing!!!\n")
+              playagain = False
+       sys.exit(" ☘️ Bye !!")
 
 
 
